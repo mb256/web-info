b9dcd4c7ea8a9d31fb13aaa17e69e48bede4c9bc
Michal Broucek <michal.broucek@siemens.com>
2025-10-09 16:14:16 +0200

    Introduce route app

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import os
 
 app = Flask(__name__, static_folder='assets', static_url_path='/static_web')
@@ -16,9 +16,23 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/welcome')
-def welcome():
-    return render_template('welcome.html')
+@app.route('/route')
+def route():
+    return render_template('route.html')
+
+
+@app.route('/search', methods=['POST'])
+def search_route():
+    route_name = request.form.get('route_name')
+    if route_name:
+        # TODO:
+        # Here you can add your search logic
+        # For now, we'll just pass the search term to a results template
+        return render_template('route.html', search_query=route_name, show_results=True)
+    else:
+        return redirect(url_for('route'))
+
+
 
 if __name__ == '__main__':
     # For local development
